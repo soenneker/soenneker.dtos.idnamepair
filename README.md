1a1a501bc7adb33f14609e2b2c6fd3be1a42af46
Jake Soenneker <jake@soenneker.com>
2023-10-31 20:57:22 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Standby...
+[![](https://img.shields.io/nuget/v/Soenneker.Dtos.IdNamePair.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Dtos.IdNamePair/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.dtos.idnamepair/publish.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.dtos.idnamepair/actions/workflows/publish.yml)
+[![](https://img.shields.io/nuget/dt/Soenneker.Dtos.IdNamePair.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Dtos.IdNamePair/)
+
+# ![](https://user-images.githubusercontent.com/4441470/224455560-91ed3ee7-f510-4041-a8d2-3fc093025112.png) Soenneker.Dtos.IdNamePair
+### A minimal Record type with an Id (string), Name (string), and maximum JSON compatibility
+
+## Installation
+
+```
+Install-Package Soenneker.Dtos.IdNamePair
+```
---
+++ src/IdNamePair.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace Soenneker.Dtos.IdNamePair;
+
+/// <summary>
+/// A minimal Record type with an <see cref="Id"/> (string), <see cref="Name"/> (string), and maximum JSON compatibility. <para/>
+/// The reason for this type (over something like KeyValuePair, dictionary) is because of JSON serialization support.
+/// </summary>
+/// <remarks>Record type, so avoid modification after initial construction.</remarks>
+[DataContract]
+public record IdNamePair
+{
+    [Required]
+    [DataMember(Name = "id")]
+    [JsonPropertyName("id")]
+    [JsonProperty("id")]
+    public string Id { get; set; } = default!;
+
+    [Required]
+    [DataMember(Name = "name")]
+    [JsonPropertyName("name")]
+    [JsonProperty("name")]
+    public string Name { get; set; } = default!;
+}
