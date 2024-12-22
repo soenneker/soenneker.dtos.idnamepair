826b612a43e1296a3c625fcd9da1652266b8d3a6
Jake Soenneker <jake@soeneker.com>
2024-12-22 13:58:56 +0000

    Tests

---
+++ test/Soenneker.Dtos.IdNamePair.Tests/IdNamePairTests.cs
@@ -0,0 +1,18 @@
+using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Dtos.IdNamePair.Tests;
+
+[Collection("Collection")]
+public class IdNamePairTests : FixturedUnitTest
+{
+    public IdNamePairTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
